2449cde9c4f19fa27744e3842a6527fc2cd4affa
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 12:44:29 -0500

    Adding two 2D matrices.

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
+"""Adding two matrices element-wise"""
 
+
+def add_matrices2D(mat1, mat2):
+    """Adds two 2D matrices element-wise."""
+
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
+        sum = []
+        for i in range(len(mat1)):
+            row_sum = []
+            for j in range(len(mat1[0])):
+                row_sum.append(mat1[i][j] + mat2[i][j])
+            sum.append(row_sum)
+
+        return sum
