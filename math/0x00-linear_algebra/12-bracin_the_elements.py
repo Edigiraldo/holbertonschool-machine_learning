b3a168a8f9961f42900782f377eda31af4305c69
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 16:00:45 -0500

    numpy arrays element-wise operations.

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+"""numpy arrays element-wise operations."""
 
+
+def np_elementwise(mat1, mat2):
+    """function that performs element-wise addition,
+    subtraction, multiplication, and division"""
+
+    a = mat1 + mat2
+    s = mat1 - mat2
+    m = mat1 * mat2
+    d = mat1 / mat2
+
+    return a, s, m, d
