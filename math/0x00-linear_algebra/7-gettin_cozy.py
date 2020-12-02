ffd8d647447409c9311c6c38e04f48e54c2cbe60
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 13:17:11 -0500

    Concatenating two 2D matrices along an specific axis.

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
+"""Concatenating two 2D matrices along an specific axis."""
 
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """function that concatenates two matrices along a specific axis."""
+
+    concat = []
+    mat1_cpy = []
+    for row in mat1:
+        mat1_cpy.append(row.copy()) 
+    concat.extend(mat1_cpy)
+
+    mat2_cpy = []
+    for row in mat2:
+        mat2_cpy.append(row.copy()) 
+
+    if axis == 0:
+        concat.extend(mat2_cpy)
+    elif axis == 1:
+        for i in range(len(mat2)):
+            concat[i].extend(mat2_cpy[i])
+
+    return concat
