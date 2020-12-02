33a61913e353566363edbdd143e993df2a43c5f7
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 16:15:11 -0500

    concatenating ndarrays along specific axes.

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
+"""concatenating ndarrays."""
+import numpy as np
 
+
+def np_cat(mat1, mat2, axis=0):
+    """function that concatenates two matrices along a specific axis."""
+
+    return np.concatenate((mat1, mat2), axis=axis)
