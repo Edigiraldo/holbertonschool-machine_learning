8e4a92302e086f15ae611baf8aae4d25d4267920
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 12:30:19 -0500

    Adding lists element-wise.

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
+"""Adds two arrays element-wise"""
 
+
+def add_arrays(arr1, arr2):
+    """Adds two arrays element by element."""
+    if len(arr1) == len(arr2):
+        sum = []
+        len1 = len(arr1)
+        for idx in range(len1):
+            sum.append(arr1[idx] + arr2[idx])
+        return sum
