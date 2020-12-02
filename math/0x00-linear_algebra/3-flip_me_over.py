5bc2de18b332b9e96bb8cf919c48ae273359d9de
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 12:22:02 -0500

    Transposing 2D matrices.

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+"""Transpose a matrix."""
 
+
+def matrix_transpose(matrix):
+    """Returns the transpose of an 2D matrix."""
+    transpose = []
+    for col in range(len(matrix[0])):
+        column = []
+        for row in range(len(matrix)):
+            column.append(matrix[row][col])
+        transpose.append(column)
+
+    return transpose
