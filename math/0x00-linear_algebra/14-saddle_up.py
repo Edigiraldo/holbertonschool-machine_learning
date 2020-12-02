1fd2d2de8cac4ab84725a4fa76c5407d283b3da0
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 16:24:09 -0500

    matricial multiplication with numpy arrays.

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
+"""Matrix multiplication with numpy arrays."""
 
+
+def np_matmul(mat1, mat2):
+    """function that performs matrix multiplication."""
+
+    return mat1 @ mat2
