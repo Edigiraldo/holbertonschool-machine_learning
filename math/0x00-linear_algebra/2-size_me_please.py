58f5d6ecb7bf4c767acebcbc0d9f1bac6a082202
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 12:07:19 -0500

    size of a matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
+"""Getting a matrix shape."""
 
+
+def matrix_shape(matrix):
+    """Returns the shape of matrix."""
+    shape_list = []
+    shape(matrix, shape_list)
+
+    return shape_list
+
+
+def shape(matrix, shape_list):
+    """shape of the matrix"""
+    if type(matrix) == list and len(matrix) > 0:
+        shape_list.append(len(matrix))
+        shape(matrix[0], shape_list)
