d5815d544df27bccf673eaa2ee0f4bc8aa2ba99f
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 12:49:14 -0500

    concatenating two lists.

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
+"""Concatenating lists."""
 
+
+def cat_arrays(arr1, arr2):
+    """function that concatenates two arrays."""
+
+    concat = []
+    concat.extend(arr1)
+    concat.extend(arr2)
+
+    return concat
