56679b7575f88c2793862e3fa755c5306aa5ae83
Edison Girado A <1470@Holbertonschool.com>
2020-12-02 20:23:42 -0500

    slicing along a specific axis.

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+"""Slicing along a specific axis."""
 
+
+def np_slice(matrix, axes={}):
+    """function that slices a matrix along a specific axes."""
+
+    slc = [slice(None)] * len(matrix.shape)
+
+    for axis in axes.keys():
+        ax = int(axis)
+        slc[ax] = slice(*axes[axis])
+
+    return matrix[tuple(slc)]
