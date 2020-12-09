fc711604b1a60d092f7c5f82ae62bc2d099b1572
Edison Girado A <1470@Holbertonschool.com>
2020-12-09 13:29:33 -0500

    sum.

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Sum of squares."""
+
+def summation_i_squared(n):
+    """Calculates sum of squares from i = 1 to i = n."""
+    if n > 0:
+        sum = (n * (n + 1) * (2 * n + 1)) / 6
+        return sum
