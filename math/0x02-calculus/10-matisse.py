f70166bb15756530e099e6ee551c00d5d8940681
Edison Girado A <1470@Holbertonschool.com>
2020-12-09 13:58:20 -0500

    derivative of a polynomial.

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Deriving polinomials."""
+
+
+def poly_derivative(poly):
+    """function that calculates the derivative of a polynomial."""
+    for ele in poly:
+        if type(ele) != int and type(ele) != float:
+            return None
+
+    for i in range(len(poly)):
+        poly[i] = poly[i] * i
+
+    for i in range(len(poly)):
+        if poly[i] != 0:
+            break
+        if i == (len(poly) - 1):
+            return [0]
+
+    return poly[1:]
