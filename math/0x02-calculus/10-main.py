6751770e037fd8dd9a97058e566869891aa5e809
Edison Girado A <1470@Holbertonschool.com>
2020-12-09 16:22:28 -0500

    10 main

--- math/0x02-calculus/10-main.py
+++ math/0x02-calculus/10-main.py
@@ -2,5 +2,14 @@
 
 poly_derivative = __import__('10-matisse').poly_derivative
 
-poly = [5, 3, 0, 1]
+
+poly = [-5]
+print(poly_derivative(poly))
+poly = [-5, -3, 0, '1']
+print(poly_derivative(poly))
+poly = []
+print(poly_derivative(poly))
+poly = [5, 3, 0, 1, 0, 0]
+print(poly_derivative(poly))
+poly = 5
 print(poly_derivative(poly))
