80e9e6467bf7d3c6a2497cea0b660911c94175e3
Edison Girado A <1470@Holbertonschool.com>
2020-12-15 13:52:58 -0500

    Class for poisson distribution.

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """Poisson distribution."""
 
+
 class Poisson:
     """Class Poisson that represents a poisson distribution."""
 
     def __init__(self, data=None, lambtha=1.):
         """Class constructor."""
-        if data == None:
-            if lambtha < 0:
-                 raise ValueError("lambtha must be a positive value")
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
             if type(data) != list:
