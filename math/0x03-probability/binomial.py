f156ccbe4e12d576e3342154f265ff02ad8a7722
Edison Girado A <1470@Holbertonschool.com>
2020-12-15 20:10:06 -0500

    Class for binomial distributions.

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Binomial distribution."""
+
+
+class Binomial:
+    """Class Binomial that represents a binomial distribution."""
+    def __init__(self, data=None, n=1, p=0.5):
+        """Constructor method."""
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if p >= 1 or p <= 0:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            
+            self.p = float(1 - (variance(data) / mean))
+            self.n = int(mean / self.p)
+            self.p = float(mean/self.n)
+
+
+def variance(data):
+    """Computes the variance of a given distribution."""
+    mean = sum(data) / len(data)
+    var = 0
+
+    for ele in data:
+        var += (ele - mean) ** 2
+
+    return var / len(data)
