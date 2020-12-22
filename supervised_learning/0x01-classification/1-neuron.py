86b7847379099f98aa88d08d23e31577be8191f5
Edison Girado A <1470@Holbertonschool.com>
2020-12-22 15:22:14 -0500

    Making W, b and A private fields with getters.

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Class Neuron."""
+import numpy as np
+
+
+class Neuron:
+    """Class Neuron that defines a single neuron
+    performing binary classification."""
+    def __init__(self, nx):
+        """Class constructor."""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter for W private attribute."""
+        return self.__W
+    @property
+    def b(self):
+        """getter for b private attribute."""
+        return self.__b
+    @property
+    def A(self):
+        """getter for A private attribute."""
+        return self.__A
