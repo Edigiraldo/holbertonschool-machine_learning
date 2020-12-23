4b8ae9d0e364a444e603ebee0d8f5c944a2cd133
Edison Girado A <1470@Holbertonschool.com>
2020-12-23 13:29:00 -0500

    Creating NeuralNetwork class.

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""NeuralNetwork class."""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Class that defines a neural network with
+    one hidden layer performing binary classification"""
+    def __init__(self, nx, nodes):
+        """Class constructor."""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # hidden layer
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+
+        # output layer
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
