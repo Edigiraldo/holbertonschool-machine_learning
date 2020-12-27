eab5e2497bc87ce0c43e4f03e7e9aad3e9ce4f3e
Edison Girado A <1470@Holbertonschool.com>
2020-12-26 20:52:45 -0500

    Function that implements one hot encode.

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,18 @@
+#!/usr/bin python3
+"""One-Hot Encode."""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """function that converts a numeric
+    label vector into a one-hot matrix."""
+    if (type(Y) != np.ndarray or Y.shape[0] != Y.size
+       or Y.size == 0 or type(classes) != int
+       or classes <= 0):
+
+        return None
+
+    ohe = np.zeros((classes, Y.shape[0]))
+    ohe[Y, np.arange(classes)] = 1
+
+    return ohe
