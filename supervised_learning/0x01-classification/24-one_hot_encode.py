3e51bbe8d4a410c644338d76880a360a78280dc9
Edison Girado A <1470@Holbertonschool.com>
2021-01-03 19:27:29 -0500

    One hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -6,10 +6,8 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """function that converts a numeric
     label vector into a one-hot matrix."""
-    if (type(Y) != np.ndarray or Y.shape[0] != Y.size
-       or Y.size == 0 or type(classes) != int
-       or classes <= 0):
-
+    if (not isinstance(Y, np.ndarray) or len(Y) == 0 or
+            not isinstance(classes, int) or classes <= np.amax(Y)):
         return None
 
     ohe = np.zeros((classes, Y.shape[0]))
