8d5724520236e9f8253cac696d5a97e416eee939
Edison Girado A <1470@Holbertonschool.com>
2021-01-07 18:58:11 -0500

    Fixing pycodestyle.

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -6,7 +6,8 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """Function to create a layer."""
     Heetal = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, kernel_initializer=Heetal, activation=activation, name='layer')
+    layer = tf.layers.Dense(units=n, kernel_initializer=Heetal,
+                            activation=activation, name='layer')
     y_pred = layer(prev)
 
     return y_pred
