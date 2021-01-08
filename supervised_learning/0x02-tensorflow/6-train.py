5f3b38472b80aff65799144f44be5d6959e8d776
Edison Girado A <1470@Holbertonschool.com>
2021-01-07 22:18:28 -0500

    Function to train and save the NN.

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -13,26 +13,37 @@ def train(X_train, Y_train, X_valid, Y_valid,
           iterations, save_path="/tmp/model.ckpt"):
 
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+
     y_pred = forward_prop(x, layer_sizes, activations)
+    tf.add_to_collection('y_pred', y_pred)
+
     accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('accuracy', accuracy)
+
     loss = calculate_loss(y, y_pred)
+    tf.add_to_collection('loss', loss)
+
     train_op = create_train_op(loss, alpha)
+    tf.add_to_collection('train_op', train_op)
 
     saver = tf.train.Saver()
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
         sess.run(init)
-        for i in range(iterations):
-            _, accuracy_t, loss_value_t = sess.run((train_op, accuracy, loss), feed_dict={x: X_train, y: Y_train})
+        for i in range(iterations + 1):
+            accuracy_t, loss_value_t = sess.run((accuracy, loss), feed_dict={x: X_train, y: Y_train})
             accuracy_v, loss_value_v = sess.run((accuracy, loss), feed_dict={x: X_valid, y: Y_valid})
             if i % 100 == 0:
-                print("After {i} iterations:")
                 print("After {} iterations:".format(i))
                 print("\tTraining Cost: {}".format(loss_value_t))
                 print("\tTraining Accuracy: {}".format(accuracy_t))
                 print("\tValidation Cost: {}".format(loss_value_v))
                 print("\tValidation Accuracy: {}".format(accuracy_v))
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
         saver.save(sess, save_path)
 
