3fe99582d2d6c7ec9cd9d42e12350aef938cc5e0
Edison Girado A <1470@Holbertonschool.com>
2021-01-07 20:27:20 -0500

    Implementing function to calculate accuracy.

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def calculate_accuracy(y, y_pred):
     """Function that calculates the
     accuracy of a prediction."""
-    equality = tf.equal(y, y_pred)
-    accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
-
-    return accuracy
+    y_true = tf.argmax(y, axis=1)
+    y_pred = tf.argmax(y_pred, axis=1)
+    acc = tf.reduce_mean(tf.cast(tf.equal(y_true, y_pred), dtype=tf.float32))
+    return acc
