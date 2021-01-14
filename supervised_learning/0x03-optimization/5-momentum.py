f04c06b65a07b44786f54cf3fad9f35dcdaf616c
Edison Girado A <1470@Holbertonschool.com>
2021-01-13 19:46:57 -0500

    Fixing style.

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -6,5 +6,5 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
     """Function that updates a variable using the
     gradient descent with momentum optimization algorithm."""
     momentum = beta1 * v + (1 - beta1) * grad
-    
+
     return var - alpha * momentum, momentum
