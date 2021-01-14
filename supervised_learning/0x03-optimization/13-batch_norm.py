c84ec6f165d24380d8a4795e36687dd526345d99
Edison Girado A <1470@Holbertonschool.com>
2021-01-14 16:18:15 -0500

    batch normalization.

---
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function batch_norm."""
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """Function that normalizes an unactivated output
+    of a neural network using batch normalization."""
+    
+    Z_norm =  (Z - np.mean(Z, axis=0, keepdims=True))
+    Z_norm /= np.sqrt(np.var(Z, axis=0, keepdims=True) + epsilon)
+
+    Z_batch_norm = gamma * Z_norm + beta
+
+    return Z_batch_norm
