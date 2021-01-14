e57dc6eea6cc07e4f2fb0e8a08f2c4cb8b8eddd2
Edison Girado A <1470@Holbertonschool.com>
2021-01-14 16:19:56 -0500

    Fixing pycodestyle.

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -6,8 +6,7 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """Function that normalizes an unactivated output
     of a neural network using batch normalization."""
-    
-    Z_norm =  (Z - np.mean(Z, axis=0, keepdims=True))
+    Z_norm = (Z - np.mean(Z, axis=0, keepdims=True))
     Z_norm /= np.sqrt(np.var(Z, axis=0, keepdims=True) + epsilon)
 
     Z_batch_norm = gamma * Z_norm + beta
