5436af0ed13c5cc6cbf9ab7e9b95a27485afcbbd
Edison Girado A <1470@Holbertonschool.com>
2021-01-13 19:45:58 -0500

    Momentum gradient descent.

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Funciton update_variables_momentum."""
+"""Function update_variables_momentum."""
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """Function that updates a variable using the
     gradient descent with momentum optimization algorithm."""
-    momentum = beta1 * v + alpha * grad
-    return var - momentum, momentum
+    momentum = beta1 * v + (1 - beta1) * grad
+    
+    return var - alpha * momentum, momentum
