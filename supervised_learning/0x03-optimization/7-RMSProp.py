0d6c80b19a83732a5aa7cbd6b335df5195fb8605
Edison Girado A <1470@Holbertonschool.com>
2021-01-13 21:07:44 -0500

    Changing epsilon position.

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -6,6 +6,6 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Funciton that updates a variable using
     the RMSProp optimization algorithm."""
     S = beta2 * s + (1 - beta2) * (grad ** 2)
-    var = var - alpha * grad / ((S + epsilon) ** 0.5)
+    var = var - alpha * grad / (S ** 0.5 + epsilon)
 
     return var, S
