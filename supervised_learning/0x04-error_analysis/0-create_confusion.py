373f74a98549a6f81ee6099eb1a74032fbff9c61
Edison Girado A <1470@Holbertonschool.com>
2021-01-18 17:58:43 -0500

    Fixing MemoryError.

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -6,11 +6,14 @@ import numpy as np
 def create_confusion_matrix(labels, logits):
     """Function that creates a confusion matrix."""
     m = labels.shape[0]
-    confusion = np.zeros((m, m))
+    classes = labels.shape[1]
+
+    confusion = np.zeros((classes, classes))
 
     real = np.argmax(labels, axis=1)
     predicted = np.argmax(logits, axis=1)
 
-    confusion[real, predicted] += 1
+    for i in range(m):
+        confusion[real[i], predicted[i]] += 1
 
     return confusion
