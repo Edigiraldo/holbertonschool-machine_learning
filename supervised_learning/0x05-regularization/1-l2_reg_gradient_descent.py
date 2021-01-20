82c39824f9eee1f446f2dae5eaeaaab7ff458160
Edison Girado A <1470@Holbertonschool.com>
2021-01-20 16:29:13 -0500

    Gradient descent with l2 reg.

---
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Function l2_reg_gradient_descent."""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """Function that updates the weights and biases of a neural
+    network using gradient descent with L2 regularization."""
+    m = Y.shape[1]
+    weights_c = weights.copy()
+    for i in range(L, 0, -1):
+        w_k = 'W' + str(i)
+        b_k = 'b' + str(i)
+        a_k = 'A' + str(i)
+        A = cache[a_k]
+        if i == L:
+            dz = A - Y
+        else:
+            dz = (1 - A ** 2) * np.matmul(
+                        weights_c["W{}".format(i + 1)].T, dz)
+
+        weights[w_k] = weights[w_k] - alpha * (lambtha / m) * weights[w_k]
+        - alpha * (np.matmul(dz, cache["A{}".format(i - 1)].T) / m)
+        weights[b_k] = weights[b_k]
+        - alpha * (np.sum(dz, axis=1, keepdims=True) / m)
