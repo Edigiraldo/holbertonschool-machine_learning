ae7f166a7601e3c437e917ef867395ae061bbeef
Edison Girado A <1470@Holbertonschool.com>
2021-01-26 17:47:53 -0500

    Including function to Train the NN.

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -2,6 +2,7 @@
 """Function train_model."""
 import tensorflow.keras as K
 
+
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """Function that trains a model using
