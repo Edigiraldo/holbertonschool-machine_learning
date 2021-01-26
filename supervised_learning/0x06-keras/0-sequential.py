08a8dd2f45572492d728188a99f12cd1f6f231c5
Edison Girado A <1470@Holbertonschool.com>
2021-01-26 16:13:05 -0500

    Including 1 layer case.

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -16,7 +16,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                              kernel_regularizer=ker_reg,
                              input_shape=(nx,)))
 
-    model.add(K.layers.Dropout(1 - keep_prob))
+    if n_layers != 1:
+        model.add(K.layers.Dropout(1 - keep_prob))
 
     for i in range(1, n_layers):
         ker_reg = K.regularizers.l2(lambtha)
