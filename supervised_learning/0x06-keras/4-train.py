280b3ed8e5a198709844667283cb7012b606051f
Edison Girado A <1470@Holbertonschool.com>
2021-01-26 18:40:35 -0500

    Including function to Train the NN.

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Function train_model."""
-import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size,
-                epochs, verbose=False, shuffle=False):
+                epochs, verbose=True, shuffle=False):
     """Function that trains a model using
     mini-batch gradient descent."""
     hist = network.fit(x=data,
