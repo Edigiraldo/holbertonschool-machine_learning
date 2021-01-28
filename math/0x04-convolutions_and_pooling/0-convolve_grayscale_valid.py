74c5fb2c47ce0777fdf0ca031e24b08dc64266b4
Edison Girado A <1470@Holbertonschool.com>
2021-01-28 17:04:28 -0500

    Function to apply a filter to an image.

---
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Function convolve_grayscale_valid."""
+import numpy as np
+from math import ceil, floor
+
+
+def convolve_grayscale_valid(images, kernel):
+    """Function that performs a valid
+    convolution on grayscale images."""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    out_h = h - kh + 1
+    out_w = w - kw + 1
+
+    out = np.zeros((m, out_h, out_w))
+    for i in range(out_h):
+        for j in range(out_w):
+            img = images[:, i: i + kh, j: j  + kw]
+            out[:, i, j] = (kernel[np.newaxis, :] * img).sum()
+
+    return out
