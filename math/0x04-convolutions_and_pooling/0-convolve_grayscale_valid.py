aee6fc21b3ba38a2d8de410dc23f5b5794b96911
Edison Girado A <1470@Holbertonschool.com>
2021-01-28 17:33:27 -0500

    Fixing sum over axis error.

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -16,6 +16,7 @@ def convolve_grayscale_valid(images, kernel):
     for i in range(out_h):
         for j in range(out_w):
             img = images[:, i: i + kh, j: j + kw]
-            out[:, i, j] = (kernel[np.newaxis, :] * img).sum()
+            out[:, i, j] = (kernel[np.newaxis, :] * img)\
+                .sum(axis=2).sum(axis=1)
 
     return out
