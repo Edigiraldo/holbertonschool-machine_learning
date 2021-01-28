80520596de58ed57af4c6419125eee91322e7b0f
Edison Girado A <1470@Holbertonschool.com>
2021-01-28 17:09:32 -0500

    Fixing pycodestyle.

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Function convolve_grayscale_valid."""
 import numpy as np
-from math import ceil, floor
 
 
 def convolve_grayscale_valid(images, kernel):
@@ -16,7 +15,7 @@ def convolve_grayscale_valid(images, kernel):
     out = np.zeros((m, out_h, out_w))
     for i in range(out_h):
         for j in range(out_w):
-            img = images[:, i: i + kh, j: j  + kw]
+            img = images[:, i: i + kh, j: j + kw]
             out[:, i, j] = (kernel[np.newaxis, :] * img).sum()
 
     return out
