92b06544fd0d3b66a941a1a605fb9c10b66b4f2e
Edison Girado A <1470@Holbertonschool.com>
2021-02-03 15:22:55 -0500

    convolution forward.

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -10,24 +10,29 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     kh, kw, c, nc = W.shape
     sh, sw = stride
 
-    if padding == "same":
+    if type(padding) == tuple:
+        ph, pw = padding
+
+    elif padding == "same":
         ph = int(((h - 1) * sh + kh - h) / 2) + 1
         pw = int(((w - 1) * sw + kw - w) / 2) + 1
-    else:  # padding == "valid"
+
+    elif padding == "valid":
         ph, pw = 0, 0
 
     images_padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                            'constant')
 
-    out_h = int(((h + 2 * ph - kh) / sh) + 1)
-    out_w = int(((w + 2 * pw - kw) / sw) + 1)
+    out_h = int(((h + (2*ph) - kh) / sh) + 1)
+    out_w = int(((w + (2*pw) - kw) / sw) + 1)
     out = np.zeros((m, out_h, out_w, nc))
 
     for i in range(out_h):
         for j in range(out_w):
             img = images_padded[:, i * sh: i * sh + kh,
                                 j * sw: j * sw + kw, :, np.newaxis]
-            out[:, i, j] = np.sum(W[np.newaxis, :] * img, axis=(3, 2, 1))
+            out[:, i, j] = (W[np.newaxis, :] * img)\
+                .sum(axis=1).sum(axis=2).sum(axis=1)
 
     out = activation(out + b)
 
