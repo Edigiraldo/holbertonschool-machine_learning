4bfb5b0336f549cd7c2378753a4585cbf102f6a4
Edison Girado A <1470@Holbertonschool.com>
2021-02-03 14:56:33 -0500

    changing sum.

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -27,8 +27,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         for j in range(out_w):
             img = images_padded[:, i * sh: i * sh + kh,
                                 j * sw: j * sw + kw, :, np.newaxis]
-            out[:, i, j] = (W[np.newaxis, :] * img)\
-                .sum(axis=3).sum(axis=2).sum(axis=1)
+            out[:, i, j] = np.sum(W[np.newaxis, :] * img, axis=(3, 2, 1))
 
     out = activation(out) + b
 
