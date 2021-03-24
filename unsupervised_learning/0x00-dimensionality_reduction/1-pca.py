25310fa38d4385a963ce089994eec8a7af3abf6e
Edison Girado A <1470@Holbertonschool.com>
2021-03-24 17:27:08 -0500

    PCA function.

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""pca function."""
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Function that performs PCA on a dataset.
+
+    - X is a numpy.ndarray of shape (n, d) where:
+        - n is the number of data points
+        - d is the number of dimensions in each point
+    - ndim is the new dimensionality of the transformed X
+
+    Returns: T, a numpy.ndarray of shape (n, ndim)
+             containing the transformed version of X.
+    """
+    U, S, Vh = np.linalg.svd(X, full_matrices=False)
+
+    T = U[:, :ndim] @ np.diag(S)[:ndim, :ndim]
+
+    return T
