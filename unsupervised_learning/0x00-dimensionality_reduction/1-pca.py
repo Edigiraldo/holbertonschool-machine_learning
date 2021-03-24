b6f4be270d68f1eeadb9d421741e6117a0387860
Edison Girado A <1470@Holbertonschool.com>
2021-03-24 18:17:22 -0500

    pca

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -22,10 +22,8 @@ def pca(X, ndim):
     """
     X = X - np.mean(X, axis=0)
     _, S, V = np.linalg.svd(X)
-    idx = S.argsort()[::-1]
-    eig_val = S[idx]
+    eig_val = S
     eig_vect = V.T
-    eig_vect = eig_vect[:, idx]
 
     w = eig_vect[:, 0: ndim]
     T = np.matmul(X, w)
