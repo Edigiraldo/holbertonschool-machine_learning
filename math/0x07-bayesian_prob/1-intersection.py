695cbad9ad3c88b7f69c591254cc9da4304142bd
Edison Girado A <1470@Holbertonschool.com>
2021-03-25 15:03:03 -0500

    Fixing raising errors order.

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -62,13 +62,21 @@ def intersection(x, n, P, Pr):
     Returns: a 1D numpy.ndarray containing the intersection of obtaining
     x and n with each probability in P, respectively.
     """
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        msg = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(msg)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-
     n_Pr = Pr.shape[0]
     n_P = P.shape[0]
     if type(Pr) is not np.ndarray or Pr.ndim != 1 or n_Pr != n_P:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if (P < 0).any() or (P > 1).any():
+        raise ValueError("All values in P must be in the range [0, 1]")
     if (Pr < 0).any() or (Pr > 1).any():
         raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose(Pr.sum(), 1):
