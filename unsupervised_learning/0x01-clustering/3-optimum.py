1a1d7449b42bed1f4650c3a2a5d0f61ffe79391d
Edison Girado A <1470@Holbertonschool.com>
2021-03-28 15:34:32 -0500

    Implementing optimun function.

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Function optimum_k."""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Function that tests for the optimum number of clusters
+    by variance.
+
+    - X is a numpy.ndarray of shape (n, d) containing the data set.
+    - kmin is a positive integer containing the minimum number
+      of clusters to check for (inclusive).
+    - kmax is a positive integer containing the maximum number
+      of clusters to check for (inclusive).
+    - iterations is a positive integer containing the maximum
+      number of iterations for K-means.
+
+    Returns: (results, d_vars), or (None, None) on failure.
+        - results is a list containing the outputs of K-means
+          for each cluster size.
+        - d_vars is a list containing the difference in variance
+          from the smallest cluster size for each cluster size.
+    """
+    results = []
+    variances = []
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        if C is None or clss is None:
+            return None, None
+        results.append((C, clss))
+        var = variance(X, C)
+        if var is None:
+            return None, None
+        variances.append(var)
+
+    variances = [-1 * (variances[i] - variances[0])
+                 for i in range(len(variances))]
+
+    return results, variances
