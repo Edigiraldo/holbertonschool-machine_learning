c0448b96ade9d2fcca358ac0b0027dadfaf42a6f
Edison Girado A <1470@Holbertonschool.com>
2021-03-27 21:06:24 -0500

    Implementing initialize function.

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Function initialize."""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    Function that initializes cluster centroids for K-means.
+
+    - X is a numpy.ndarray of shape (n, d) containing the
+      dataset that will be used for K-means clustering.
+        - n is the number of data points.
+        - d is the number of dimensions for each data point.
+    - k is a positive integer containing the number of clusters.
+
+    Returns: a numpy.ndarray of shape (k, d) containing the
+    initialized centroids for each cluster, or None on failure.
+    """
+    if type(X) != np.ndarray or X.ndim != 2:
+        return None
+
+    low = X.min(axis=0)
+    high = X.max(axis=0)
+    d = X.shape[1]
+    init = np.random.uniform(low=low, high=high, size=(k, d))
+
+    return init
