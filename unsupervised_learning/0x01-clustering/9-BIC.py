67d9ca0fde42a5dd08bfcf67d23da90e30bd33ee
Edison Girado A <1470@Holbertonschool.com>
2021-03-31 14:17:57 -0500

    Implementing BIC function.

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """Function BIC."""
+import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
-def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):    """
-    Function 
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """
+    Function that finds the best number of clusters for a GMM
+    using the Bayesian Information Criterion.
 
     - X is a numpy.ndarray of shape (n, d) containing the data set.
     - kmin is a positive integer containing the minimum number of
