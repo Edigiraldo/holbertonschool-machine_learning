3550b7dc512ae55ba5198257aef80b7715001497
Edison Girado A <1470@Holbertonschool.com>
2021-05-19 15:13:36 -0500

    Implementing SelfAttention class.

---
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -0,0 +1,46 @@
+#!/usr/bin env python3
+"""Class SelfAttention."""
+import tensorflow as tf
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    """Class to calculate the attention for machine translation."""
+    def __init__(self, units):
+        """
+        Class constructor.Sets the following public instance attributes:
+            - W - a Dense layer with units units, to be applied to the
+              previous decoder hidden state.
+            - U - a Dense layer with units units, to be applied to the
+              encoder hidden states.
+            - V - a Dense layer with 1 units, to be applied to the tanh
+              of the sum of the outputs of W and U.
+
+        - units is an integer representing the number of hidden units in
+          the alignment model.
+        """
+        self.W = tf.keras.layers.Dense(units)
+        self.U = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def __call__(self, s_prev, hidden_states):
+        """
+        - s_prev is a tensor of shape (batch, units) containing the
+          previous decoder hidden state.
+        - hidden_states is a tensor of shape (batch, input_seq_len,
+          units)containing the outputs of the encoder.
+
+        Returns: context, weights.
+            - context is a tensor of shape (batch, units) that
+              contains the context vector for the decoder.
+            - weights is a tensor of shape (batch, input_seq_len, 1)
+              that contains the attention weights.
+        """
+        s_prev = s_prev[:, tf.newaxis, :]
+
+        alig = self.V(tf.nn.tanh(self.W(s_prev) + self.U(hidden_states)))
+        weights = tf.nn.softmax(alig, axis=1)
+
+        context = weights * hidden_states
+        context = tf.reduce_sum(context, axis=1)
+
+        return context, weights
