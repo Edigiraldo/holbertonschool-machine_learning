1ed8a2bca4763db010da6dc7894fc871e92bc889
Edison Girado A <1470@Holbertonschool.com>
2021-07-30 15:51:28 -0500

    Implementing script that lists all bands with Glam rock as their main style, ranked by their longevity.

---
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -0,0 +1,2 @@
+-- script that lists all bands with Glam rock as their main style, ranked by their longevity.
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style like "%Glam rock%" ORDER BY lifespan DESC;
