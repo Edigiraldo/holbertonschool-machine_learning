deb6147157196ad1a6a6f741ab3f11cba2d6353d
Edison Girado A <1470@Holbertonschool.com>
2021-07-31 12:25:11 -0500

    Changing update.

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -3,7 +3,7 @@ DELIMITER $$ ;
 CREATE PROCEDURE ComputeAverageScoreForUser (IN u_id int)
 BEGIN
   DECLARE avg_score int;
-  SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = u_id GROUP BY user_id);
+  SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = u_id);
   UPDATE users SET average_score = avg_score WHERE id = u_id;
 END$$
 DELIMITER ; $$
