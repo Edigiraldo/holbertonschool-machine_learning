99e968a7ebf4f4671fbe9fa0f5b3d4a3478d50aa
Edison Girado A <1470@Holbertonschool.com>
2021-07-31 12:30:37 -0500

    Changing delimiter statements.

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -1,9 +1,9 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
-DELIMITER $$ ;
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN u_id int)
 BEGIN
   DECLARE avg_score int;
   SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = u_id);
   UPDATE users SET average_score = avg_score WHERE id = u_id;
 END$$
-DELIMITER ; $$
+DELIMITER ;
