47442403e47707568f77f7e673a1dd99a158b5e2
Edison Girado A <1470@Holbertonschool.com>
2021-08-02 16:25:57 -0500

    Implementing function schools_by_topic

---
+++ pipeline/0x02-databases/33-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Function ."""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function that returns the list of school having a specific topic.
+
+    - mongo_collection: pymongo collection object.
+    - topics: topic searched.
+
+    Return: list of school having a specific topic.
+    """
+    l_results = []
+    results = mongo_collection.find({"topics": {"$all": [topic]}})
+    for result in results:
+        l_results.append(result)
+    return l_results
